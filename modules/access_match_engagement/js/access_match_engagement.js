b24dd0494831ec598f5ed9fb08d55d08a493bffb
Miles France <miles.france@protitude.com>
2022-07-26 15:29:58 +0000

    phpcs fixes

--- modules/access_match_engagement/js/access_match_engagement.js
+++ modules/access_match_engagement/js/access_match_engagement.js
@@ -2,11 +2,11 @@ var checkBoxPromo = document.getElementById("edit-promote-value");
 var checkBoxEmail = document.getElementById("edit-field-email-user-value");
 
 function checkBoth(chkBoxId, checkBoxWhich) {
-  if (checkBoxWhich.checked == true){
+  if (checkBoxWhich.checked == TRUE){
     console.log('checked');
-    document.getElementById(chkBoxId).checked = true;
+    document.getElementById(chkBoxId).checked = TRUE;
   }
 }
 
-checkBoxPromo.onclick = function(){checkBoth('edit-field-email-user-value', checkBoxPromo);};
-checkBoxEmail.onclick = function(){checkBoth('edit-promote-value', checkBoxEmail);};
+checkBoxPromo.onclick = function () {checkBoth('edit-field-email-user-value', checkBoxPromo);};
+checkBoxEmail.onclick = function () {checkBoth('edit-promote-value', checkBoxEmail);};
