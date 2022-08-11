3b6b77a7b51bc0078d04c94338d437e05e1bc916
Miles France <miles.france@protitude.com>
2022-08-11 23:40:25 +0000

    match_engagement_view: update js file

--- modules/access_match_engagement/js/match_engagement_view.js
+++ modules/access_match_engagement/js/match_engagement_view.js
@@ -1,31 +1,31 @@
-articles = document.querySelectorAll('article');
-for (let i = 0; i < articles.length; i++) { 
-  articles[i].insertAdjacentHTML('beforeend', '<button type="button" data-article="' + i + '" class="btn btn-primary ml-0 mt-3 more-match">+ More</button>');
+rows = document.querySelectorAll('.view-content .row');
+for (let i = 0; i < rows.length; i++) { 
+  rows[i].insertAdjacentHTML('beforeend', '<button type="button" data-article="' + i + '" class="btn btn-primary ml-0 mt-3 more-match">+ More</button>');
 }
 
 var more = document.getElementsByClassName("more-match");
 var showMore = function() {
     var attribute = this.getAttribute("data-article");
-    if (articles[attribute].querySelector('.field--type-text-with-summary').classList.contains('visually-hidden')) {
-      var hasSummary = articles[attribute].getElementsByClassName('field--type-text-with-summary');
+    if (rows[attribute].querySelector('.field--type-text-with-summary').classList.contains('visually-hidden')) {
+      var hasSummary = rows[attribute].getElementsByClassName('field--type-text-with-summary');
       if (hasSummary.length > 0) {
-        articles[attribute].querySelector('.field--type-text-with-summary').classList.remove("visually-hidden");
+        rows[attribute].querySelector('.field--type-text-with-summary').classList.remove("visually-hidden");
       }
-      var hasTags = articles[attribute].getElementsByClassName('field--name-field-tags');
+      var hasTags = rows[attribute].getElementsByClassName('field--name-field-tags');
       if (hasTags.length > 0) {
-        articles[attribute].querySelector('.field--name-field-tags').classList.remove("visually-hidden");
+        rows[attribute].querySelector('.field--name-field-tags').classList.remove("visually-hidden");
       }
-      articles[attribute].querySelector('.more-match').innerHTML = "- Less";
+      rows[attribute].querySelector('.more-match').innerHTML = "- Less";
     } else {
-      var hasSummary = articles[attribute].getElementsByClassName('field--type-text-with-summary');
+      var hasSummary = rows[attribute].getElementsByClassName('field--type-text-with-summary');
       if (hasSummary.length > 0) {
-        articles[attribute].querySelector('.field--type-text-with-summary').classList.add("visually-hidden");
+        rows[attribute].querySelector('.field--type-text-with-summary').classList.add("visually-hidden");
       }
-      var hasTags = articles[attribute].getElementsByClassName('field--name-field-tags');
+      var hasTags = rows[attribute].getElementsByClassName('field--name-field-tags');
       if (hasTags.length > 0) {
-        articles[attribute].querySelector('.field--name-field-tags').classList.add("visually-hidden");
+        rows[attribute].querySelector('.field--name-field-tags').classList.add("visually-hidden");
       }
-      articles[attribute].querySelector('.more-match').innerHTML = "+ More";
+      rows[attribute].querySelector('.more-match').innerHTML = "+ More";
     }
 };
 for (var i = 0; i < more.length; i++) {
