334673d1c989d865e19efa9d7596aa02e7517546
Andrew Pasquale <andrew@elytra.net>
2022-08-29 23:02:39 -0400

    ACCESS News block - Request News

---
+++ modules/access_news/src/Plugin/Block/RequestNewsBlock.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\access_news\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'Request News' Block.
+ *
+ * @Block(
+ *   id = "requestnews_block",
+ *   admin_label = @Translation("Request a News Post block"),
+ *   category = @Translation("ACCESS"),
+ * )
+ */
+class RequestNewsBlock extends BlockBase {
+
+/**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+    return [
+      '#theme' => 'requestnews_block',
+    ];
+  }
+
+}
