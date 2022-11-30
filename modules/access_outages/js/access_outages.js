b8673f30305868ee549076b6665b60283ebaef7a
Jasper Lieber <jasper.amp@gmail.com>
2022-11-30 17:56:45 +0000

    for AG outages, only show planned outages if there are any

--- modules/access_outages/js/access_outages.js
+++ modules/access_outages/js/access_outages.js
@@ -32,10 +32,6 @@ document.onreadystatechange = function () {
  */
 const showAgOutages = async function showAgOutages(ciderIds, bDebugWithAllOutages) {
 
-  // planned outages are always shown -- create the html for this,
-  // with placeholder text
-  addOutageTableHtmlToDom()
-
   // make the api calls and show results
   showCurrentOutages(ciderIds, bDebugWithAllOutages)
   showPlannedOutages(ciderIds, bDebugWithAllOutages)
@@ -44,15 +40,14 @@ const showAgOutages = async function showAgOutages(ciderIds, bDebugWithAllOutage
 /**
  * Add the planned outages table to the DOM
  */
-function addOutageTableHtmlToDom(){
+ function addOutageTableHtmlToDom(){
 
   let outagesTableDiv = document.createElement('div')
   outagesTableDiv.innerHTML = `<br>
     <div class="outage-list section container">
       <div class="row">
         <div class="mb-3">
-          <h3 class="border-bottom pb-2">Planned Downtimes</h3>
-          <p id="no-planned-outages">(Retrieving planned outages for the Associated Infrastructure)</p>
+          <h3 class="pb-2">Planned Downtimes</h3>
           <div class="table-responsive">
             <table id="outages-planned" class="display text-start table" style="display:none;">
               <thead>
@@ -73,7 +68,6 @@ function addOutageTableHtmlToDom(){
   container = document.getElementById('access_news')
   container.appendChild(outagesTableDiv, container.firstChild);
 }
-
 
 /**
  * Current outages are shown in boxes in a div at top of page.  
@@ -142,8 +136,8 @@ function  getOutageHtml(outage) {
 }
 
 /**
- * Planned outages are shown in the previously created html datatable.  
- * Make the API call and filter results against the listin ciderIds and 
+ * Planned outages are shown in an html datatable.  
+ * Make the API call and filter results against the list in ciderIds and 
  * fill the the datatable
  * 
  * @param {*} ciderIds 
@@ -160,11 +154,11 @@ const showPlannedOutages = async function showPlannedOutages(ciderIds, bDebugWit
   let outages = await response.json()
   let filtered = filterOutages(outages, ciderIds)
 
-  const noOutages = document.getElementById(`no-planned-outages`)
-  if (filtered.length == 0) {
-    noOutages.innerHTML = 'There are no planned outages for Associated Infrastructure' 
-  } else {
-    noOutages.style.display = 'none' 
+  if (filtered.length > 0) {
+
+    // create the html table 
+    addOutageTableHtmlToDom()
+
     const outagesTable = document.getElementById('outages-planned')
     const options = {
       timeZoneName: 'short'
@@ -191,7 +185,8 @@ const showPlannedOutages = async function showPlannedOutages(ciderIds, bDebugWit
       ],
       order: [[1, 'desc']], // sort by OutageStart
       bPaginate: false,
-      bAutoWidth: false
+      bAutoWidth: false,
+      searching: false
     })
     outagesTable.style.display = 'block'
   }
@@ -208,7 +203,7 @@ const showPlannedOutages = async function showPlannedOutages(ciderIds, bDebugWit
 function filterOutages(outages, ciderIds) {
   let filtered = []
   Object.keys( outages ).forEach( function( key ) {
-    if(ciderIds.indexOf(outages[key]['ResourceID']) > -1) {
+    if (ciderIds.indexOf(outages[key]['ResourceID']) > -1) {
         filtered.push(outages[key]);
     }
   });
