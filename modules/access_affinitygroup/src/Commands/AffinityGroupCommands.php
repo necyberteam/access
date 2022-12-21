c6cf001e0915a217d6a319d2a4c7509d7c88df2a
Andrew Pasquale <andrew@elytra.net>
2022-12-21 18:31:57 +0000

    Check CC API if user already exists

--- modules/access_affinitygroup/src/Commands/AffinityGroupCommands.php
+++ modules/access_affinitygroup/src/Commands/AffinityGroupCommands.php
@@ -39,8 +39,8 @@ class AffinityGroupCommands extends DrushCommands {
       // If there isn't a Constant Contact list_id,
       // trigger save to generate Constant Contact list.
       $list_id = $node->get('field_list_id')->value;
-      // $this->output()->writeln($list_id);
-      if (!$list_id) {
+      $this->output()->writeln($list_id);
+      if (!$list_id || strlen($list_id) < 1) {
         $node->save();
         $list_id = $node->get('field_list_id')->value;
       }
@@ -51,7 +51,7 @@ class AffinityGroupCommands extends DrushCommands {
       $flags = $flag_service->getAllEntityFlaggings($term->entity);
       foreach ($flags as $flag) {
         $uid = $flag->get('uid')->target_id;
-        // $this->output()->writeln($uid);
+        $this->output()->writeln($uid);
         $user = User::load($uid);
 
         $first_name = $user->get('field_user_first_name')->getString();
@@ -61,15 +61,28 @@ class AffinityGroupCommands extends DrushCommands {
         $first_name = substr($first_name, 0, 49);
         $last_name = substr($last_name, 0, 49);
 
+        $this->output()->writeln($first_name);
+        $this->output()->writeln($last_name);
+
         // Get the Constant Contact id for the User.
         $field_val = $user->get('field_constant_contact_id')->getValue();
-        if (!empty($field_val)) {
+        if (!empty($field_val) && $field_val != 0) {
           $cc_id = $field_val[0]['value'];
-          $this->output()->writeln($first_name . ' ' . $last_name . ' already has cc id.');
+          $this->output()->writeln($first_name . ' ' . $last_name . ' already has cc id: ' . $cc_id);
         }
         else {
-          // If user did not already have the CC id, try to add to CC.
-          $cc_id = $cca->addContact($first_name, $last_name, $user->getEmail());
+          // User did not already have the CC id.
+          // Check if they are already in CC.
+          $resp = $cca->apiCall('/contacts?status=all&email=' . $user->getEmail());
+          if ($resp->contacts) {
+            $cc_id = $resp->contacts[0]->contact_id;
+            $this->output()->writeln($cc_id);
+          }
+          else {
+            // Try to add to CC.
+            $cc_id = $cca->addContact($first_name, $last_name, $user->getEmail());
+          }
+          $this->output()->writeln($cc_id);
           $user->set('field_constant_contact_id', $cc_id);
           $user->save();
           $this->output()->writeln('Added ' . $first_name . ' ' . $last_name);
@@ -80,6 +93,7 @@ class AffinityGroupCommands extends DrushCommands {
           ],
           'list_ids' => [$list_id],
         ];
+        // $this->output()->writeln(var_dump($post_data));
         $cca->apiCall('/activities/add_list_memberships', json_encode($post_data), 'POST');
       }
     }
