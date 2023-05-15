6fbc059db2a98d6aeb59a592766d91e9c1459edf
Andrew Pasquale <andrew@elytra.net>
2023-05-15 18:47:56 +0000

    Fix for urlencoding display name

--- modules/ticketing/src/Controller/TicketingController.php
+++ modules/ticketing/src/Controller/TicketingController.php
@@ -4,24 +4,32 @@ namespace Drupal\ticketing\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Routing\TrustedRedirectResponse;
+use Drupal\Core\Url;
+use Drupal\user\Entity\User;
 
 /**
- * redirect to JSM
+ * Redirect to JSM.
  */
 class TicketingController extends ControllerBase {
 
   /**
-   * Redirect to JSM, and prefill the 
+   * Redirect to JSM, and prefill the .
    */
   public function doRedirect() {
-    $account = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
+    $account = User::load(\Drupal::currentUser()->id());
     $account_name = $account->getAccountName();
     $display_name = $account->getDisplayName();
 
-    $uri = 'https://access-ci.atlassian.net/servicedesk/customer/portal/2/group/3/create/17?customfield_10103='
-        . urlencode($account_name) .'&customfield_10108='
-        . urlencode($display_name);
+    $uri = Url::fromUri('https://access-ci.atlassian.net/servicedesk/customer/portal/2/group/3/create/17',
+      [
+        'query' => [
+          'customfield_10103' => $account_name,
+          'customfield_10108' => $display_name,
+        ],
+      ]
+    );
 
-    return new TrustedRedirectResponse($uri);
+    return new TrustedRedirectResponse($uri->toString());
   }
+
 }
