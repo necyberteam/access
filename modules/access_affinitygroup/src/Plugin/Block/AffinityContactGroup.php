ad71229d4641a56219ec1871af17971f3842b8d6
protitude <miles.france@gmail.com>
2023-05-19 16:53:08 +0000

    block: added contact button - module: set perms d8-1455

---
+++ modules/access_affinitygroup/src/Plugin/Block/AffinityContactGroup.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\access_affinitygroup\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Cache\Cache;
+
+/**
+ * Provides a button to contact affinity group.
+ *
+ * @Block(
+ *   id = "affinity_contact_group",
+ *   admin_label = "Affinity Contact Group",
+ * )
+ */
+class AffinityContactGroup extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $nid = \Drupal::routeMatch()->getParameter('node')->id();
+    $contact['string'] = [
+      '#type' => 'inline_template',
+      '#template' => '<a class="bg-secondary btn cursor-default text-white m-2" href="/form/affinity-group-contact?nid={{ nid }}">{{ contact_text }}</a>',
+      '#context' => [
+        'contact_text' => $this->t('Contact Affinity Group'),
+        'nid' => $nid,
+      ],
+    ];
+
+    return $contact;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheTags() {
+    if ($node = \Drupal::routeMatch()->getParameter('node')) {
+      return Cache::mergeTags(parent::getCacheTags(), array('node:' . $node->id()));
+    } else {
+      return parent::getCacheTags();
+    }
+  }
+
+  public function getCacheContexts() {
+    return Cache::mergeContexts(parent::getCacheContexts(), array('route'));
+  }
+
+}
