7962228518102b552a6a0a87fb9bbdaab76a8c02
Andrew Pasquale <andrew@elytra.net>
2023-07-13 11:34:27 +0200

    Update access_match_engagement.js
    Check for variables before using

--- modules/access_match_engagement/js/access_match_engagement.js
+++ modules/access_match_engagement/js/access_match_engagement.js
@@ -1,7 +1,11 @@
 var notes = document.getElementById("edit-field-notes-to-author-wrapper");
-notes.classList.add("hide");
+if (notes) {
+  notes.classList.add("hide");
+}
 
 var checkBoxEmail = document.getElementById("edit-field-email-user-value");
-checkBoxEmail.onclick = function () {
-  notes.classList.toggle("hide");
-};
+if (checkBoxEmail) {
+  checkBoxEmail.onclick = function () {
+    notes.classList.toggle("hide");
+  };
+}
