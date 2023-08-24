db0997c87629b59abc54ed6cafb63ab4e4351b34
protitude <miles.france@gmail.com>
2023-08-24 19:32:35 +0000

    CiCommunity: Expire block in 24 hours d8-1511

--- modules/access_affinitygroup/src/Plugin/Block/CiCommunity.php
+++ modules/access_affinitygroup/src/Plugin/Block/CiCommunity.php
@@ -66,7 +66,11 @@ class CiCommunity extends BlockBase {
       $list .= '</ul>';
     }
     return [
-      ['#markup' => $list],
+      '#markup' => $list,
+      '#cache' => [
+        // Expire in one day in seconds.
+        'max-age' => 86400,
+      ],
     ];
   }
 
