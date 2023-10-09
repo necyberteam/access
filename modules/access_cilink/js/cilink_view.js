420d4c8052e53ac3924bc733a193c838fe3ccc57
Andrew Pasquale <andrew@elytra.net>
2023-10-09 02:46:29 +0000

    Rename resource_view to cilink_view

---
+++ modules/access_cilink/js/cilink_view.js
@@ -0,0 +1,7 @@
+// Get all elements with the class "flag-upvote a"
+const flagUpvoteElements = document.querySelectorAll('.flag-upvote a');
+
+// Loop through each element and add the class "text-decoration-none"
+flagUpvoteElements.forEach(element => {
+  element.classList.add('text-decoration-none', 'fw-normal');
+});
