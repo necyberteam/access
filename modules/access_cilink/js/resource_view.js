76fcff9154a62b733fa4a1893f74ec8c6acd11c2
Andrew Pasquale <andrew@elytra.net>
2023-10-09 02:47:15 +0000

    Another file renaming resource to cilink

--- modules/access_cilink/js/resource_view.js
+++
@@ -1,7 +0,0 @@
-// Get all elements with the class "flag-upvote a"
-const flagUpvoteElements = document.querySelectorAll('.flag-upvote a');
-
-// Loop through each element and add the class "text-decoration-none"
-flagUpvoteElements.forEach(element => {
-  element.classList.add('text-decoration-none', 'fw-normal');
-});
