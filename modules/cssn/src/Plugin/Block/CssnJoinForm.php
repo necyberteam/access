a5d759080adb0a7aea18d497cfc1654260e7d630
protitude <miles.france@gmail.com>
2023-10-13 21:00:20 +0000

    cssn: block: adding cssnjoinform d8-1876

---
+++ modules/cssn/src/Plugin/Block/CssnJoinForm.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Drupal\cssn\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Cache\Cache;
+
+/**
+ * Provides Join CSSN webform in block.
+ *
+ * @Block(
+ *   id = "cssn_join_form",
+ *   admin_label = "Display webform to join CSSN",
+ * )
+ */
+class CssnJoinForm extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $join_login = '';
+    $join_webform = '';
+    $user = \Drupal::currentUser();
+    if ($user->isAnonymous()) {
+      // Create drupal 9 link to /user/login?redirect=/cssn#join-cssn with text Login to join CSSN, with classes 'btn' and 'btn-primary'.
+      $join_login = [
+        '#type' => 'link',
+        '#title' => 'Login to join CSSN',
+        '#url' => \Drupal\Core\Url::fromRoute('user.login', [], ['query' => ['redirect' => '/cssn#join-cssn']]),
+        '#attributes' => [
+          'class' => ['md--mt-16', 'btn', 'btn-primary'],
+        ],
+      ];
+    }
+    else {
+      $webform = \Drupal::entityTypeManager()->getStorage('webform')->load('join_the_cssn_network');
+      $join_webform = $webform->getSubmissionForm();
+    }
+    $join_img = [
+      '#theme' => 'image',
+      '#uri' => 'public://cssn/join-cssn.svg',
+      '#alt' => 'Join the CSSN Network',
+      '#attributes' => [
+        'class' => ['hidden md--block'],
+      ],
+    ];
+    $join_img_mobile = [
+      '#theme' => 'image',
+      '#uri' => 'public://cssn/join-cssn-mobile.svg',
+      '#alt' => 'Join the CSSN Network',
+      '#attributes' => [
+        'class' => ['block md--hidden'],
+      ],
+    ];
+
+    $block['string'] = [
+      '#type' => 'inline_template',
+      '#template' => '<div id="join-cssn" class="items-center bg-md-teal grid grid-cols-1 md--grid-cols-2 gap-5 p-10">
+        <div>
+          {{ join_img }} {{ join_img_mobile }}
+        </div>
+        <div class="[&>*]--text-white [&_.form-checkboxes]--flex-col">
+          {{ join_login }} {{ join_webform }}
+        </div>
+      </div>',
+      '#context' => [
+        'join_webform' => $join_webform,
+        'join_login' => $join_login,
+        'join_img' => $join_img,
+        'join_img_mobile' => $join_img_mobile,
+      ],
+    ];
+    return $block;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheTags() {
+    if ($user = \Drupal::currentUser()) {
+      return Cache::mergeTags(parent::getCacheTags(), ['user:' . $user->id()]);
+    }
+    else {
+      return parent::getCacheTags();
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheContexts() {
+    return Cache::mergeContexts(parent::getCacheContexts(), ['user']);
+  }
+
+}
