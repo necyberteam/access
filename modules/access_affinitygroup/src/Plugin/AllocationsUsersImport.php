a6f4e12275f8abe716937c0a88407177e4dbe97d
Andrew Pasquale <andrew@elytra.net>
2023-10-25 14:37:12 +0000

    accessCheck for entityQuery

--- modules/access_affinitygroup/src/Plugin/AllocationsUsersImport.php
+++ modules/access_affinitygroup/src/Plugin/AllocationsUsersImport.php
@@ -623,12 +623,14 @@ class AllocationsUsersImport {
 
   /**
    * Find the access organization entity reference for the given access org id.
-   *  This is the field that is on the user profile.
+   *
+   * This is the field that is on the user profile.
    */
   private function findAccessOrg($accessOrgId) {
     $query = \Drupal::entityQuery('node')
       ->condition('type', 'access_organization')
       ->condition('field_organization_id', $accessOrgId)
+      ->accessCheck(FALSE)
       ->execute();
 
     return $query;
