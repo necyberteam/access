7b93c03a45f7c8757923dd7a9b42cdf85cd0f8bc
protitude <miles.france@gmail.com>
2024-01-26 22:16:49 +0000

    Just button event handlers should be in the once()

--- modules/access_misc/js/custom_tags.js
+++ modules/access_misc/js/custom_tags.js
@@ -1,55 +1,7 @@
 Drupal.behaviors.nodeAddTags = {
   attach: function (context, settings) {
     once('custom_tags', 'html').forEach( element => {
-      var tid_values = [];
-
-      if (Drupal.ajax) {
-
-        var tagWrapper = document.getElementById("field-tags-replace");
-        var suggested_tids = tagWrapper.getAttribute('data-suggest').split(', ');
-        var selectElementTags = document.getElementById("edit-tags--wrapper");
-
-        if (suggested_tids != 0) {
-          Array.from(suggested_tids).forEach(function (suggested_tid) {
-            if (selectElementTags.querySelector('input[type=checkbox][value="' + suggested_tid + '"]').checked != true) {
-              // selectElementTags.querySelector('option[value="' + suggested_tid + '"]').selected = true;
-              tid_values.push(suggested_tid);
-              selectElementTags.querySelector('input[type=checkbox][value="' + suggested_tid +'"]').checked = true;
-            }
-          });
-          addToList();
-        }
-      }
-
-
-
-
-      /* Show list of selected tags */
-      function addToList() {
-          // Select all elements with the 'selected' class
-          const selectedElements = document.querySelectorAll('.tags-select.selected');
-          // Initialize an array to store the text content
-          const textArray = [];
-          // Loop through each selected element and add its text content to the array
-          selectedElements.forEach(element => {
-            textArray.push(element.textContent.trim());
-          });
-          if (textArray.length > 0) {
-            var divElement = document.getElementById('tag-suggstions');
-            var textTagListing = '';
-            for (let textArrayItem of textArray) {
-              textTagListing = textTagListing + '<a class="font-normal text-sky-900" href="#tag-' + textArrayItem + '">' + textArrayItem  + '</a>, '
-            }
-            // Replace the text content
-            divElement.innerHTML = '<div class="bg-slate-100 p-5 my-5"><strong>Selected Tags:</strong><br/>' + textTagListing + '</div>';
-          }
-      }
-
-
-
-
       const buttons = document.getElementsByClassName('tags-select');
-      // const selected = [];
 
       // When button is selected add selected class and check the corresponding checkbox.
       const buttonPressed = e => {
@@ -66,33 +18,80 @@ Drupal.behaviors.nodeAddTags = {
             e.target.classList.add("selected");
           }
         }
-        addToList();
+        updateSelectedTagList();
       }
 
       document.addEventListener('DOMContentLoaded', function () {
-        for(let button of buttons) {
+        for (let button of buttons) {
           button.addEventListener("click", buttonPressed, false);
         }
       });
-
-      var checkboxes = document.querySelectorAll('input[type=checkbox]');
-        // Array to store the values of the selected checkboxes.
-        // looping through all checkboxes
-        for(var i = 0; i<checkboxes.length; i++) {
-        if (checkboxes[i].checked) {
-          tid_values.push(checkboxes[i].value);
-        }
-      }
-      // Select buttons with the same tid as the selected checkboxes.
-      for (let tid_value of tid_values) {
-        currentButton = document.querySelectorAll("[data-tid='" + tid_value + "']");
-        // If a title is selected then the button won't exist.
-        if (currentButton.length > 0) {
-          currentButton[0].classList.add("selected");
-        }
-      }
-
-      addToList();
     });
+
+    var tid_values = [];
+
+    if (Drupal.ajax) {
+
+      var tagWrapper = document.getElementById("field-tags-replace");
+      var suggested_tids = tagWrapper.getAttribute('data-suggest').split(', ');
+      var selectElementTags = document.getElementById("edit-tags--wrapper");
+
+      if (suggested_tids != 0) {
+        Array.from(suggested_tids).forEach(function (suggested_tid) {
+          if (selectElementTags.querySelector('input[type=checkbox][value="' + suggested_tid + '"]').checked != true) {
+            tid_values.push(suggested_tid);
+            selectElementTags.querySelector('input[type=checkbox][value="' + suggested_tid +'"]').checked = true;
+          }
+        });
+        updateSelectedTagList();
+      }
+    }
+
+    /* Show list of selected tags */
+    function updateSelectedTagList() {
+      // Select all elements with the 'selected' class
+      const selectedElements = document.querySelectorAll('.tags-select.selected');
+      // Initialize an array to store the text content
+      const textArray = [];
+      // Loop through each selected element and add its text content to the array
+      selectedElements.forEach(element => {
+        textArray.push(element.textContent.trim());
+      });
+      if (textArray.length > 0) {
+        var divElement = document.getElementById('tag-suggestions');
+        var textTagListing = '';
+        for (let textArrayItem of textArray) {
+          textTagListing = textTagListing + '<a class="font-normal text-sky-900" href="#tag-' + textArrayItem + '">' + textArrayItem + '</a>, '
+        }
+        // Remove the last comma and space
+        textTagListing = textTagListing.slice(0, -2);
+        // Replace the text content
+        divElement.innerHTML = '<div class="bg-slate-100 p-5 my-5"><strong>Selected Tags:</strong><br/>' + textTagListing + '</div>';
+      } else {
+        // no tags selected
+        var divElement = document.getElementById('tag-suggestions');
+        divElement.innerHTML = '';
+      }
+    }
+
+    var checkboxes = document.querySelectorAll('input[type=checkbox]');
+    // Array to store the values of the selected checkboxes.
+    // looping through all checkboxes
+    for(var i = 0; i<checkboxes.length; i++) {
+      if (checkboxes[i].checked) {
+        tid_values.push(checkboxes[i].value);
+      }
+    }
+    // Select buttons with the same tid as the selected checkboxes.
+    for (let tid_value of tid_values) {
+      currentButton = document.querySelectorAll("[data-tid='" + tid_value + "']");
+      // If a title is selected then the button won't exist.
+      if (currentButton.length > 0) {
+        currentButton[0].classList.add("selected");
+      }
+    }
+
+    updateSelectedTagList();
+
   }
-};
+}
