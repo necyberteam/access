1267e963ffa39d1c3f725613217411535da8beff
protitude <miles.france@gmail.com>
2024-03-08 15:12:07 -0700

    access_cilink: js add clipboard copy for url - add button to copy to clipboard d8-1957

--- modules/access_cilink/js/resource_view.js
+++ modules/access_cilink/js/resource_view.js
@@ -5,3 +5,23 @@ const flagUpvoteElements = document.querySelectorAll('.flag-upvote a');
 flagUpvoteElements.forEach(element => {
   element.classList.add('text-decoration-none', 'fw-normal');
 });
+
+function copyclip(url, event) {
+  navigator.clipboard.writeText(url);
+
+  // Get the button that was clicked.
+  var clickedButton = event.currentTarget;
+
+  // Get the span elements within the clicked button.
+  var copyDefault = clickedButton.querySelector('.default-message');
+  var copySuccess = clickedButton.querySelector('.copied-message');
+
+  // Add 'hidden' class to the default message and remove it from the success message.
+  copyDefault.classList.add('hidden');
+  copySuccess.classList.remove('hidden')
+  // After 3 seconds, remove the 'hidden' class from the default message and add it to the success message.
+  setTimeout(function() {
+    copyDefault.classList.remove('hidden');
+    copySuccess.classList.add('hidden');
+  }, 3000);
+}
