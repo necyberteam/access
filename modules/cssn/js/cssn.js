ff2351decf6e8d0edab239e6c50d813836041e9a
protitude <miles.france@gmail.com>
2024-03-21 17:05:11 -0600

    cssn: update button for user views d8-1989

--- modules/cssn/js/cssn.js
+++ modules/cssn/js/cssn.js
@@ -18,8 +18,8 @@ function bioLess() {
 
 // Add the button to the bio summary on user profile for campus champions.
 setTimeout(function () {
-  const summaryElement = document.querySelector('.campus-champions #bio-summary');
+  const summaryElement = document.querySelector('.user-profile-view #bio-summary');
   summaryElement.innerHTML += "<button id='bio-more' onclick='bioMore()' style='border-width: 0 !important;' class='btn btn-primary p-3'aria-hidden='TRUE' type='button'><i class='fa-solid fa-chevron-down'></i> More</button>";
-  const fullBioElement = document.querySelector('.campus-champions #full-bio');
+  const fullBioElement = document.querySelector('.user-profile-view #full-bio');
   fullBioElement.innerHTML += "<button id='bio-less' onclick='bioLess()' style='border-width: 0 !important;' class='btn btn-primary p-3' aria-hidden='TRUE' type='button'><i class='fa-solid fa-chevron-up'></i> Less</button>";
 }, 500);
