c7004120469c866f3426ad13f587f1e5a0503633
protitude <miles.france@gmail.com>
2024-03-21 14:05:26 -0600

    add more to campus champions bio on user profile d8-1989

--- modules/cssn/js/cssn.js
+++ modules/cssn/js/cssn.js
@@ -15,3 +15,11 @@ function bioLess() {
   // Add the hidden class
   summary.classList.remove('hidden');
 }
+
+// Add the button to the bio summary on user profile for campus champions.
+setTimeout(function () {
+  const summaryElement = document.querySelector('.campus-champions #bio-summary');
+  summaryElement.innerHTML += "<button id='bio-more' onclick='bioMore()' style='border-width: 0 !important;' class='btn btn-primary p-3'aria-hidden='TRUE' type='button'><i class='fa-solid fa-chevron-down'></i> More</button>";
+  const fullBioElement = document.querySelector('.campus-champions #full-bio');
+  fullBioElement.innerHTML += "<button id='bio-less' onclick='bioLess()' style='border-width: 0 !important;' class='btn btn-primary p-3' aria-hidden='TRUE' type='button'><i class='fa-solid fa-chevron-up'></i> Less</button>";
+}, 500);
