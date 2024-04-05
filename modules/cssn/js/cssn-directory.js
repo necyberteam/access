294edc40dde6e5c14545395bf2a37aa89f620c7f
protitude <miles.france@gmail.com>
2024-04-05 10:47:28 -0600

    cssn: update the tag count d8-2048

--- modules/cssn/js/cssn-directory.js
+++ modules/cssn/js/cssn-directory.js
@@ -4,7 +4,8 @@ item.forEach(addCount);
 function addCount(item) {
   // count .square-tag li inside index
   var squareTags = item.querySelectorAll('li').length;
-  if (squareTags > 0) {
+  if (squareTags > 5) {
+    var squareTags = squareTags - 5;
     item.querySelector('.count').innerHTML = "+ " + squareTags + " more";
   }
 }
