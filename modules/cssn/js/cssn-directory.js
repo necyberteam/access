75ff6c54b6642c32644d23d66beb7d3ac351a1b4
protitude <miles.france@gmail.com>
2024-04-05 12:36:49 -0600

    cssn: rework count js d8-2048

--- modules/cssn/js/cssn-directory.js
+++ modules/cssn/js/cssn-directory.js
@@ -1,11 +1,13 @@
 
-var item = document.querySelectorAll('.border-orange-hover');
+var item = document.querySelectorAll('.cssn-directory-item');
 item.forEach(addCount);
 function addCount(item) {
   // count .square-tag li inside index
   var squareTags = item.querySelectorAll('li').length;
   if (squareTags > 5) {
     var squareTags = squareTags - 5;
-    item.querySelector('.count').innerHTML = "+ " + squareTags + " more";
+    var more = "+ " + squareTags + " more";
+    item.querySelector('li:last-child').innerHTML = more;
+    item.querySelector('li:last-child').style.display = "inline-block";
   }
 }
