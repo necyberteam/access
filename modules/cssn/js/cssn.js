271bea8e5e6209d84d6398cd03916992c36ec0a9
protitude <miles.france@gmail.com>
2024-04-19 12:33:22 -0600

    cssn: fix more button

--- modules/cssn/js/cssn.js
+++ modules/cssn/js/cssn.js
@@ -18,7 +18,7 @@ function bioLess() {
 
 // Add the button to the bio summary on user profile for campus champions.
 setTimeout(function () {
-  const summaryElement = document.querySelector('.user-profile-view #bio-summary');
+  const summaryElement = document.querySelector('.user-profile-view #bio-summary.more');
   summaryElement.innerHTML += "<button id='bio-more' onclick='bioMore()' style='border-width: 0 !important;' class='btn btn-primary p-3'aria-hidden='TRUE' type='button'><i class='fa-solid fa-chevron-down'></i> More</button>";
   const fullBioElement = document.querySelector('.user-profile-view #full-bio');
   fullBioElement.innerHTML += "<button id='bio-less' onclick='bioLess()' style='border-width: 0 !important;' class='btn btn-primary p-3' aria-hidden='TRUE' type='button'><i class='fa-solid fa-chevron-up'></i> Less</button>";
