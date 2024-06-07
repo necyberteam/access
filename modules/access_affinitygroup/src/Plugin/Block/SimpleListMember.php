70d2cd4fb539d3349480e534caed321aff15ce5e
protitude <miles.france@gmail.com>
2024-06-07 11:41:11 -0600

    access_misc: moved copyclip js here - access_affinitygroup: update member email list block

--- modules/access_affinitygroup/src/Plugin/Block/SimpleListMember.php
+++ modules/access_affinitygroup/src/Plugin/Block/SimpleListMember.php
@@ -59,6 +59,11 @@ class SimpleListMember extends BlockBase implements
     // Load current node.
     $node = \Drupal::routeMatch()->getParameter('node');
     $simple_list_enabled = $node->get('field_use_ext_email_list')->value;
+
+    if (!$simple_list_enabled) {
+      return [];
+    }
+
     $group_slug = $node->get('field_group_slug')->value;
     // Get current user email.
     $current_user = \Drupal::currentUser();
@@ -89,11 +94,31 @@ class SimpleListMember extends BlockBase implements
     }
     $simple['string'] = [
       '#type' => 'inline_template',
+      'lib' => [
+        '#attached' => [
+          'library' => [
+            'access_misc/copyclip',
+          ],
+        ],
+      ],
       '#template' => '<div class="bg-light-teal px-4 pt-4 mb-10 block block-layout-builder block-inline-blockbasic">
         <div class="clearfix text-formatted field field--name-body field--type-text-with-summary field--label-hidden field__item">
           <h3 class="border-bottom pb-2 me-3 mr-3 mt-0">
             {{ block_title }}
           </h3>
+          <div class="d-flex flex items-center">
+            <span class="font-bold text-sm">access-support@lists.com</span>
+            <button class="copyclip text-sm ms-4 top-4 right-28 z-10" onclick="copyclip(\'access-support@lists.com\', event)">
+              <span class="default-message block leading-5 text-dark-teal">
+                <svg class="svg-inline--fa fa-link" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="link" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" data-fa-i2svg=""><path fill="currentColor" d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z"></path></svg><!-- <i class="fa-solid fa-link"></i> --><br>
+                {{ copy }}
+              </span>
+              <span class="copied-message text-dark-teal hidden d-none">
+                <svg class="svg-inline--fa fa-check" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"></path></svg><!-- <i class="fa-solid fa-check"></i> --><br>
+                Copied!
+              </span>
+            </button>
+          </div>
           <details class="bg-white">
             <summary class="bg-yellow font-bold">
               {{ list_default.title }}
@@ -108,14 +133,13 @@ class SimpleListMember extends BlockBase implements
       </div>',
       '#context' => [
         'block_title' => t('Member Email List'),
+        'copy' => t('Copy'),
         'list_default' => $list_default,
         'options' => $options,
       ],
     ];
 
-    $output = $simple_list_enabled ? $simple : [];
-
-    return $output;
+    return $simple;
   }
 
   /**
--- modules/access_cilink/js/resource_view.js
+++ modules/access_cilink/js/resource_view.js
@@ -6,22 +6,3 @@ flagUpvoteElements.forEach(element => {
   element.classList.add('text-decoration-none', 'fw-normal');
 });
 
-function copyclip(url, event) {
-  navigator.clipboard.writeText(url);
-
-  // Get the button that was clicked.
-  var clickedButton = event.currentTarget;
-
-  // Get the span elements within the clicked button.
-  var copyDefault = clickedButton.parentElement.parentElement.querySelector('.default-message');
-  var copySuccess = clickedButton.parentElement.parentElement.querySelector('.copied-message');
-
-  // Add 'hidden' class to the default message and remove it from the success message.
-  copyDefault.classList.add('hidden', 'd-none');
-  copySuccess.classList.remove('hidden', 'd-none')
-  // After 3 seconds, remove the 'hidden' class from the default message and add it to the success message.
-  setTimeout(function() {
-    copyDefault.classList.remove('hidden', 'd-none');
-    copySuccess.classList.add('hidden', 'd-none');
-  }, 6000);
-}
