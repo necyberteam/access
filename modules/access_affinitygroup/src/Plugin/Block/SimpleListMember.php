1612a9526833217194b4c6d4202249f2d748ac15
protitude <miles.france@gmail.com>
2024-07-02 17:41:53 +0000

    SimpleLists membership uses <input> element

--- modules/access_affinitygroup/src/Plugin/Block/SimpleListMember.php
+++ modules/access_affinitygroup/src/Plugin/Block/SimpleListMember.php
@@ -112,7 +112,7 @@ class SimpleListMember extends BlockBase implements
             {{ block_title }}
           </h3>
           <div class="d-flex flex items-center">
-            <span class="simplelists-email font-bold text-sm">{{ email }}</span>
+            <input type="text" class="simplelists-email text-sm" value="{{ email }}" readonly />
             <button class="copyclip simplelists-copyclip text-sm ms-4 z-10" onclick="copyclip(\'{{ email }}\', event)">
               <span class="default-message block leading-5 text-dark-teal">
                 <svg class="svg-inline--fa fa-link" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="link" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" data-fa-i2svg=""><path fill="currentColor" d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z"></path></svg><!-- <i class="fa-solid fa-link"></i> --><br>
@@ -124,7 +124,7 @@ class SimpleListMember extends BlockBase implements
               </span>
             </button>
           </div>
-          <details class="bg-white">
+          <details class="bg-white relative position-relative">
             <summary class="bg-yellow font-bold">
               {{ list_default.title }}
             </summary>
